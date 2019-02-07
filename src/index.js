c3236b453b6a5d013e9cc0254d0ed5cdc56c12d0
Dmitry Iovlev <iovlev.d@gmail.com>
2019-02-07 15:04:28 +0300

    Добавляет адаптивность

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import './scss/style.scss';
 // } else if (process.env.NODE_ENV === 'development') {
 //   console.log('Development mode');
 // }
+
+const navButton = document.getElementById('nav-toggle');
+const navList = document.getElementById('nav-list');
+
+navButton.onclick = function () {
+  navButton.classList.toggle('navigation__button--opened');
+  navList.classList.toggle('navigation__list--opened');
+}
